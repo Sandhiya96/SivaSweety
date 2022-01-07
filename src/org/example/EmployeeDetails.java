da453f2303c5a116fdcaefd61c782f3b85055bc0
Sandhiya96 <sandhuajapa@gmail.com>
2022-01-07 17:40:26 +0530

    sathya

--- src/org/example/EmployeeDetails.java
+++ src/org/example/EmployeeDetails.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class EmployeeDetails {
+	private void m3() {
+		System.out.println("Sathya doe the work");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
