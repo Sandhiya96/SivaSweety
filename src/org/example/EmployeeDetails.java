399e41da8c8bb306ab01d4c8e18512882ed0a653
Sandhiya96 <sandhuajapa@gmail.com>
2022-01-07 17:31:16 +0530

    S done the work

--- src/org/example/EmployeeDetails.java
+++ src/org/example/EmployeeDetails.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class EmployeeDetails {
+	private void m2() {
+		System.out.println("m2 created by S");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
